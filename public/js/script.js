0fa2be8244ea2b5567de3daf76f53ae0a3303bea
Petr Pridanov <p.pridanoff2016@yandex.kz>
2020-11-25 21:11:18 +0600

    Repair owl-carousel

--- public/js/script.js
+++ public/js/script.js
@@ -14,11 +14,11 @@ function sendData(data) {
     })
     .then((data) => {
       console.log(data);
-      if (data.message && data.message == "Success signin") {
+      if (data.message && (data.message == "Success signin")) {
         document.querySelector(".waiting-spinner").style = "display: flex";
         setTimeout(() => location.reload(), 500);
       }
-      if (data.message && data.message == "Success signup") {
+      if (data.message && (data.message == "Success signup")) {
         flashSpan.style = "display: flex; color: green";
         flashSpan.textContent = "Success signin";
       }
@@ -55,92 +55,84 @@ signupButton.addEventListener("click", (e) => {
 });
 
 function getGenres(data) {
-  fetch(
-    `https://api.themoviedb.org/3/genre/movie/list?api_key=${data.apiKey}&language=en-US`,
-    {
-      method: "GET",
-      cors: "no-cors",
+  fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${data.apiKey}&language=en-US`, {
+    method: 'GET',
+    cors: 'no-cors'
+  })
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
     }
-  )
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then((data) => {
-      console.log(data);
-      data.genres.forEach((element) => {
-        const li = document.createElement("li");
-        const link = document.createElement("a");
-        link.setAttribute("href", "#");
-        link.setAttribute("_id", element.id);
-        link.classList.add("dropdown-item");
-        link.textContent = element.name;
-        li.appendChild(link);
-        genresContainer.appendChild(li);
-      });
+  })
+  .then((data) => {
+    console.log(data)
+    data.genres.forEach(element => {
+      const li = document.createElement('li')
+      const link = document.createElement('a');
+      link.setAttribute('href', '#');
+      link.setAttribute('_id', element.id);
+      link.classList.add('dropdown-item')
+      link.textContent = element.name;
+      li.appendChild(link);
+      genresContainer.appendChild(li);
     });
-}
-getGenres({ apiKey: "be108878e83f4cfcd89e3a228bde78c9" });
+  })
+};
+getGenres({apiKey: 'be108878e83f4cfcd89e3a228bde78c9'})
 function getFilms(data) {
-  fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${data.apiKey}&language=en-US&page=1`,
-    {
-      method: "GET",
-      cors: "no-cors",
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${data.apiKey}&language=en-US&page=1`, {
+    method: 'GET',
+    cors: 'no-cors'
+  })
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
     }
-  )
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    data.results.forEach((el) => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      const img = document.createElement('img');
+      img.setAttribute('src', `https://image.tmdb.org/t/p/w185${el.poster_path}`);
+      div.appendChild(img);
+      const divBody = document.createElement('div');
+      divBody.classList.add('card-body');
+      const h5 = document.createElement('h5');
+      h5.classList.add('card-title');
+      h5.textContent = el.original_title;
+      divBody.appendChild(h5);
+      const overview = document.createElement('p');
+      overview.classList.add('card-text');
+      let overviewSliced = el.overview.slice(0,100);
+      if (overviewSliced.length < el.overview.length) {
+        overviewSliced += ' ';
       }
+      const link = document.createElement('a');
+      link.setAttribute('href', '#');
+      link.textContent = '->';
+      overview.textContent = overviewSliced;
+      overview.appendChild(link);
+      divBody.appendChild(overview);
+      const release = document.createElement('p');
+      release.classList.add('card-text');
+      const smallSpan = document.createElement('span');
+      smallSpan.classList.add('text-muted');
+      smallSpan.textContent = el.release_date;
+      release.appendChild(smallSpan);
+      divBody.appendChild(release);
+      div.appendChild(divBody);
+      popularContainer.appendChild(div);
     })
-    .then((data) => {
-      console.log(data);
-      data.results.forEach((el) => {
-        const div = document.createElement("div");
-        div.classList.add("card");
-        const img = document.createElement("img");
-        img.setAttribute(
-          "src",
-          `https://image.tmdb.org/t/p/w185${el.poster_path}`
-        );
-        div.appendChild(img);
-        const divBody = document.createElement("div");
-        divBody.classList.add("card-body");
-        const h5 = document.createElement("h5");
-        h5.classList.add("card-title");
-        h5.textContent = el.original_title;
-        divBody.appendChild(h5);
-        const overview = document.createElement("p");
-        overview.classList.add("card-text");
-        let overviewSliced = el.overview.slice(0, 100);
-        if (overviewSliced.length < el.overview.length) {
-          overviewSliced += " ";
-        }
-        const link = document.createElement("a");
-        link.setAttribute("href", "#");
-        link.textContent = "->";
-        overview.textContent = overviewSliced;
-        overview.appendChild(link);
-        divBody.appendChild(overview);
-        const release = document.createElement("p");
-        release.classList.add("card-text");
-        const smallSpan = document.createElement("span");
-        smallSpan.classList.add("text-muted");
-        smallSpan.textContent = el.release_date;
-        release.appendChild(smallSpan);
-        divBody.appendChild(release);
-        div.appendChild(divBody);
-      });
-    });
+  })
 }
 
-getFilms({ apiKey: "be108878e83f4cfcd89e3a228bde78c9" });
+getFilms({ apiKey: 'be108878e83f4cfcd89e3a228bde78c9'})
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-    items: 4,
-    dots: false,
-    loop: true,
+      items: 4,
+      dots: false,
+      loop: true
   });
 });
